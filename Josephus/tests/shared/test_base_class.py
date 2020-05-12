d4630124531b55f63a43ba7a9733adb437ed55e0
IVORY_C <767478058@qq.com>
2020-05-12 22:40:50 +0800

    添加对base_class.py的测试

---
+++ Josephus/tests/shared/test_base_class.py
@@ -0,0 +1,12 @@
+from main.shared import base_class as bc
+
+test_someone = bc.Person('Andy', 12, 'male')
+test_someone_age_error = bc.Person('Beth', -10, 'female')
+test_someone_gender_error = bc.Person('Cindy', 24, 'a')
+
+assert test_someone.name == 'Andy'
+assert test_someone.age == 12
+assert test_someone.gender == 'male'
+
+assert test_someone_age_error.age == -1
+assert test_someone_gender_error.gender == 'ValueError'
