0bb33e1ef2f4ef8543027380c4fdca810573cc21
IVORY_C <767478058@qq.com>
2020-05-13 18:27:05 +0800

    添加输入start和step的文本框

--- Josephus/tests/user_interface/test_ui.py
+++ Josephus/tests/user_interface/test_ui.py
@@ -7,19 +7,29 @@ class test_ui():
         self.window.move(300, 300)
         self.window.setWindowTitle('Josephus')
 
-        self.textEdit = QPlainTextEdit(self.window)
-        self.textEdit.setPlaceholderText("Please input people")
-        self.textEdit.move(10, 25)
-        self.textEdit.resize(300, 350)
+        self.people_text = QPlainTextEdit(self.window)
+        self.people_text.setPlaceholderText("Please input people")
+        self.people_text.move(10, 25)
+        self.people_text.resize(300, 350)
+
+        self.start_text = QPlainTextEdit(self.window)
+        self.start_text.setPlaceholderText("Please input start number")
+        self.start_text.move(330, 25)
+        self.start_text.resize(150, 100)
+
+        self.step_text = QPlainTextEdit(self.window)
+        self.step_text.setPlaceholderText("Please input step")
+        self.step_text.move(330, 150)
+        self.step_text.resize(150, 100)
 
         self.button = QPushButton('Result', self.window)
-        self.button.move(380, 80)
+        self.button.move(350, 300)
 
         self.button.clicked.connect(self.handle_button)
 
 
     def handle_button(self):
-        info = self.textEdit.toPlainText()
+        info = self.people_text.toPlainText()
 
         result = ''
         for line in info.splitlines():
