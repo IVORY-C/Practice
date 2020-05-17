3a72a74c59243ebebbef6020a0e74a8db2d1790b
IVORY_C <767478058@qq.com>
2020-05-17 17:43:17 +0800

    删除多余文件

--- Josephus/main/shared/serializers.py
+++
@@ -1,9 +0,0 @@
-class Reader:
-    def create_person_from_file(self):
-        raise NotImplementedError
-
-    def __len__(self):
-        raise NotImplementedError
-
-    def close_file(self):
-        raise NotImplementedError
