69f3612b9d724fcb6404657f6096169c6b23a084
IVORY_C <767478058@qq.com>
2020-05-17 10:23:56 +0800

    添加CsvReader的测试程序

---
+++ Josephus/tests/adapter/test_csvreader.py
@@ -0,0 +1,38 @@
+from main.shared import reader as rd
+from main.shared import base_class as bc
+
+class CsvReader(rd.Reader):
+    def __init__(self, path):
+        self.file = open(path, 'r', encoding='gbk')
+        self._all_data = self.file.readlines()
+
+    def create_person_from_file(self):
+        people = []
+        for each in self._all_data:
+            data = each.strip().replace(' ','').split(',')
+            name = data[0]
+            try:
+                age = int(data[1])
+            except ValueError as e:
+                age = -2
+            gender = data[2]
+            people.append(bc.Person(name, age, gender))
+            
+        return people  
+
+    def __len__(self):
+        return len(self._all_data)
+
+    def close_file(self):
+        self.file.close()
+
+
+path = 'data\\people.csv'
+csvreader = CsvReader(path)
+reader = csvreader.create_person_from_file()
+
+assert reader[0].name == 'Morty'
+assert reader[1].name == 'Rick'
+assert reader[2].age == 16
+assert reader[3].gender == 'female'
+assert reader[4].gender == 'male'
