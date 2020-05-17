7603e7a777f1e18b6608fb5052bb97d86eddcbf3
IVORY_C <767478058@qq.com>
2020-05-17 17:32:32 +0800

    修改为使用以test_开头的函数进行测试

--- Josephus/tests/adapter/test_txtreader.py
+++ Josephus/tests/adapter/test_txtreader.py
@@ -1,38 +1,15 @@
-from main.shared import reader as rd
+from main.adapter import readers as rds
 from main.shared import base_class as bc
-
-class TxtReader(rd.Reader):
-    def __init__(self, path):
-        self.file = open(path, 'r', encoding='utf-8')
-        self._all_data = self.file.readlines()
-
-    def create_person_from_file(self):
-        people = []
-        for each in self._all_data:
-            data = each.strip().replace(' ','').split(',')
-            name = data[0]
-            try:
-                age = int(data[1])
-            except ValueError as e:
-                age = -2
-            gender = data[2]
-            people.append(bc.Person(name, age, gender))
-
-        return people   
-
-    def __len__(self):
-        return len(self._all_data)
-
-    def close_file(self):
-        self.file.close()
+from typing import List
         
 
-path = 'data\\people.txt'
-txtreader = TxtReader(path)
-reader = txtreader.create_person_from_file()
-
-assert reader[0].name == 'Morty'
-assert reader[1].name == 'Rick'
-assert reader[2].age == 16
-assert reader[3].gender == 'female'
-assert reader[4].gender == 'male'
+def test_read_txt_file():
+    path = 'data\\people.txt'
+    txtreader = rds.TxtReader(path)
+    reader = txtreader.create_person_from_file()
+    
+    assert reader[0].name == 'Morty'
+    assert reader[1].name == 'Rick'
+    assert reader[2].age == 16
+    assert reader[3].gender == 'female'
+    assert reader[4].gender == 'male'
