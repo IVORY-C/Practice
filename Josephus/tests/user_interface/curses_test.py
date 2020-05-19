09597f83caeaefbd43f2d5cc0f3b4bf8d7fd965c
IVORY_C <767478058@qq.com>
2020-05-20 02:05:03 +0800

    添加读取数据或文件、约瑟夫的功能

--- Josephus/tests/user_interface/curses_test.py
+++ Josephus/tests/user_interface/curses_test.py
@@ -1,9 +1,13 @@
+import os,sys
+sys.path.append("C:\\Users\\76747\\Desktop\\Python\\practice\\Josephus")
+
 from main.shared import base_class as bc
 from main.adapter import readers as rds
 from main.use_cases import josephus as jsp
 
 import curses
 from curses.textpad import Textbox, rectangle
+from typing import List
 
 stdscr = curses.initscr()
 
@@ -15,7 +19,7 @@ def display_info(info, x, y, colorpair=2):
 def get_ch_and_continue():
     global stdscr
     stdscr.nodelay(0)
-    ch=stdscr.getch()
+    ch = stdscr.getch()
     stdscr.nodelay(1)
     return True
 
@@ -37,49 +41,65 @@ def unset_win():
     curses.echo()
     curses.endwin()
 
-def input_str(stdscr, input_note: str):
-    stdscr.addstr(0, 0, f"Enter {input_note}: (hit Ctrl-G to send)")
+def input_str(stdscr, input_note: str, start_x: int, start_y: int, size_x: int, size_y: int):
+    stdscr.addstr(start_y, start_x, f"Enter {input_note}: (hit Ctrl-G to send)")
 
-    editwin = curses.newwin(2,1, 5,30)
-    rectangle(stdscr, 1,0, 1+5+1, 1+30+1)
+    editwin = curses.newwin(size_y, size_x, start_y+1, start_x+1)
     stdscr.refresh()
 
-    box = Textbox(editwin)
+    box = Textbox(editwin, insert_mode=True)
     box.edit()
-    data = box.gather()
+    info = box.gather()
+    return info
 
-    return data
-
-
-def test_func():
+if __name__ == '__main__':
     try:
         set_win()
-        data = input_str(stdscr, 'data')
+        people_text = input_str(stdscr, 'data', 0, 1, 30, 5)
+        path = input_str(stdscr, 'path', 0, 12, 30, 2).strip()
+        file_name = input_str(stdscr, 'file_name', 0, 16, 30, 2).strip()
+        start = input_str(stdscr, 'start', 0, 20, 30, 2).strip()
+        step = input_str(stdscr, 'step', 0, 24, 30, 2).strip()
 
-        people = []
-        for each in self._all_data:
-            data = each.strip().replace(' ','').split(',')
-            name = data[0]
-            try:
+        reader = []
+        if people_text:
+            people_str: List[str] = people_text.split('\n')
+            for each in people_str:
+                if not each:
+                    continue 
+                data: List[str] = each.split(',')
+                name = data[0]
                 age = int(data[1])
-            except ValueError as e:
-                age = -2
-            gender = data[2]
-            people.append(bc.Person(name, age, gender))
-        
-        ring = jsp.Ring(people)
-        ring.reset()
-        ring.start = 2
-        ring.step = 5
-        
-        result_str = ''
-        for item in ring:
-            data_someone = line.split(',')
-            data_someone_str = f"Name: {data[0]}, Age: {data[1]}, Gender: {data[2]}"
-            result += data_str + '\n'
+                gender = data[2]
+                reader.append(bc.Person(name, age, gender))
 
-        display_info(result,0,10)
-        display_info('Press any key to continue...',0,20)
+        if path:
+            file_type = file_name.split('.')[1]
+            if file_type == 'txt':
+                file_reader = rds.TxtReader(path)
+            if file_type == 'csv':
+                file_reader = rds.CsvReader(path)
+            if file_type == 'zip':
+                file_reader = rds.ZipReader(path, file_name)
+
+            reader = file_reader.create_person_from_file()
+
+        if reader:
+            ring = jsp.Ring(reader)
+            ring.start = int(start)
+            ring.step = int(step)
+            ring.reset()
+
+            result_str = ''
+            for item in ring:
+                item_str = f"{item.name},{item.age},{item.gender}"
+                result_str += item_str + '\n'            
+        
+        else:
+            result_str = 'No data input'
+
+        stdscr.addstr(30, 0, result_str)
+        # display_info('Press any key to continue...',0,34)
         get_ch_and_continue()
     except Exception as e:
         raise e
