5f8c0b453e45f89adc1ec292be9f14467aa6f27d
IVORY_C <767478058@qq.com>
2020-05-22 15:20:20 +0800

    给对象属性添加初值，使用类属性进行错误处理

--- Josephus/domain/shared/person.py
+++ Josephus/domain/shared/person.py
@@ -1,16 +1,18 @@
 class Person(object):
     INVALID_AGE_LESS_THAN_ZERO = -1
     INVALID_AGE_IS_NOT_INT = -2
-    def __init__(self, name: str, age: int, gender: str):
+
+    INVALID_GENDER = 'ValueError'
+    def __init__(self, name: str = None, age: int = 0, gender: str = None):
         self.name = name
         self.age = age
         self.gender = gender
 
         if self.age < 0 :
-            self.age = -1
+            self.age = Person.INVALID_AGE_LESS_THAN_ZERO
 
         if self.gender not in ['male', 'female']:
-            self.gender = 'ValueError'
+            self.gender = Person.INVALID_GENDER
 
     def __eq__(self, obj) -> bool:
         if (obj.name == self.name) & (obj.age == self.age) & (obj.gender == self.gender):
