f8b29269b53382964ee83d331c870c1487d22e4a
IVORY_C <767478058@qq.com>
2020-05-26 14:34:15 +0800

    增加测试内容

--- Josephus/tests/use_cases/test_josephus.py
+++ Josephus/tests/use_cases/test_josephus.py
@@ -1,29 +1,99 @@
+import pytest
+from unittest import mock
+from collections.abc import Iterable
 
-from domain.shared import base_class as bc
+from domain.shared.person import Person
 from domain.use_cases import josephus as jsp
 
 
-def test_josephus():
-    reader = []
-    reader.append(bc.Person('Morty', 12, 'male'))
-    reader.append(bc.Person('Rick', 53, 'male'))
-    reader.append(bc.Person('Summer', 16, 'female'))
-    reader.append(bc.Person('Beth', 34, 'female'))
-    reader.append(bc.Person('Jerry', 36, 'male'))
+@pytest.fixture
+def people_example():
+    return [
+        Person('Morty', 12, 'male'),
+        Person('Rick', 53, 'male'),
+        Person('Summer', 16, 'female'),
+        Person('Beth', 34, 'female'),
+        Person('Jerry', 36, 'male')
+    ]
 
+def test_josephus_init_with_reader():
+    reader = mock.MagicMock()
     ring = jsp.Ring(reader)
+
+    reader.__iter__.assert_called_with()
+    assert ring.start == 1
+    assert ring.step == 1
+
+def test_josephus_init_with_invalid_reader():
+    reader = mock.Mock()
+    with pytest.raises(TypeError):
+        jsp.Ring(reader)
+
+def test_josephus_init_without_reader():
+    ring = jsp.Ring()
+
+    assert ring.start == 0
+    assert ring.step == 1
+    assert ring._people == []
+
+def test_josephus_append():
+    someone: Person = Person('Morty', 12, 'male')
+    ring = jsp.Ring()
+    ring.append(someone)
+    
+    assert ring._people == [someone]
+
+def test_josephus_pop(people_example):
+    ring = jsp.Ring()
+    ring._people = people_example
+    ring.pop()
+
+    assert ring._people == [
+        Person('Rick', 53, 'male'),
+        Person('Summer', 16, 'female'),
+        Person('Beth', 34, 'female'),
+        Person('Jerry', 36, 'male')
+    ]
+    
+def test_josephus_query_list(people_example):
+    ring = jsp.Ring()
+    ring._people = people_example
+    result = ring.query_list()
+
+    assert result == [
+        Person('Morty', 12, 'male'),
+        Person('Rick', 53, 'male'),
+        Person('Summer', 16, 'female'),
+        Person('Beth', 34, 'female'),
+        Person('Jerry', 36, 'male')
+    ]
+
+def test_josephus_is_iterable():
+    ring = jsp.Ring()
+
+    assert isinstance(ring, Iterable)
+
+def test_josephus_len(people_example):
+    ring = jsp.Ring()
+    ring._people = people_example
+
+    assert len(ring) == 5
+
+
+def test_josephus_output_result(people_example):
+    ring = jsp.Ring(people_example)
     ring.start = 2
     ring.step = 5
     ring.reset()
 
-    result_name = []
-    result_age =[]
-    result_gender = []
+    result = []
     for item in ring:
-        result_name.append(item.name)
-        result_age.append(item.age)
-        result_gender.append(item.gender)
+        result.append(item)
 
-    assert result_name == ['Rick', 'Summer', 'Jerry', 'Morty', 'Beth']
-    assert result_age == [53, 16, 36, 12, 34]
-    assert result_gender == ['male', 'female', 'male', 'male', 'female']
+    assert result == [
+        Person('Rick', 53, 'male'),
+        Person('Summer', 16, 'female'),
+        Person('Jerry', 36, 'male'),
+        Person('Morty', 12, 'male'),
+        Person('Beth', 34, 'female')
+    ]
