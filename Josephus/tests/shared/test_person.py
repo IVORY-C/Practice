668138eb6b7607c1f8a075e9e3282804a5bf2f81
IVORY_C <767478058@qq.com>
2020-05-26 13:45:20 +0800

    删除多余测试

--- Josephus/tests/shared/test_person.py
+++ Josephus/tests/shared/test_person.py
@@ -1,17 +1,5 @@
 from domain.shared.person import Person
 
-def test_base_class():
-    test_someone = bc.Person('Andy', 12, 'male')
-    test_someone_age_error = bc.Person('Beth', -10, 'female')
-    test_someone_gender_error = bc.Person('Cindy', 24, 'a')
-
-    assert test_someone.name == 'Andy'
-    assert test_someone.age == 12
-    assert test_someone.gender == 'male'
-
-    assert test_someone_age_error.age == -1
-    assert test_someone_gender_error.gender == 'ValueError'
-
 def test_Person_with_correct_parameters():
     someone = Person('Beth', 10, 'female')
 
